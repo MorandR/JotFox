02e7d65be303a211b842b50eb9b47282bcb72421
Richard Morand <richard@Richards-MBP.lan>
2021-05-28 22:38:00 -0400

    React Practice Design

---
+++ frontend/src/Components/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <div style = {{height: '100%', width: '100%'}}>
+            <h1> HELLO </h1>
+            <div>
+                    <p>
+                        This is Something<br />
+                        That I do <br />
+                        One plus me makes <br />
+                        Definitely not <b>YOU</b>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        
+
+                    </p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
